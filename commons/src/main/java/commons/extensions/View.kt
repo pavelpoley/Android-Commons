06170bc82001b63adefeae22649b71258ee81524
Pavel Poley <pavelpoley88@gmail.com>
2023-02-15 19:57:07 +0300

    init

--- commons/src/main/java/commons/extensions/View.kt
+++ commons/src/main/java/commons/extensions/View.kt
@@ -1,2 +1,23 @@
 package commons.extensions
 
+import android.view.View
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.visibleIf(condition: Boolean) {
+    visibility = if (condition) View.VISIBLE else View.GONE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
+
+fun View.invisibleIf(condition: Boolean) {
+    visibility = if (condition) View.INVISIBLE else View.VISIBLE
+}
